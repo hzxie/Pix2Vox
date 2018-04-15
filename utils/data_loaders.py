5e2aefb0bbf87036d1b80d45f09ed25e97a474a6
Haozhe Xie <root@haozhexie.com>
2018-04-15 19:59:48 +0800

    Complete data loader for ShapeNet.

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -17,8 +17,8 @@ from datetime import datetime as dt
 class ShapeNetDataset(torch.utils.data.dataset.Dataset):
     ''' ShapeNetDataset class used for PyTorch DataLoader
     '''
-    def __init__(self, file_list, transform=None):
-        self.file_list = file_list
+    def __init__(self, file_list_with_metadata, transform=None):
+        self.file_list = file_list_with_metadata
         self.transform = transform
 
     def __len__(self):
@@ -76,30 +76,56 @@ class ShapeNetDataLoader:
         # Load data for each category
         for taxonomy in self.dataset_taxonomy:
             taxonomy_folder_name = taxonomy['taxonomy_id']
-
             print('[INFO] %s Collecting files of Taxonomy[ID=%s, Name=%s]' % (dt.now(), taxonomy['taxonomy_id'], taxonomy['taxonomy_name']))
-            files.append(self.get_files_of_taxonomy(taxonomy_folder_name, dataset_portion, n_views))
+            files.extend(self.get_files_of_taxonomy(taxonomy_folder_name, dataset_portion, n_views))
 
+        print('[INFO] %s Complete collecting files of the dataset. Total files: %d.' % (dt.now(), len(files)))
         return ShapeNetDataset(files, transform)
 
     def get_files_of_taxonomy(self, taxonomy_folder_name, dataset_portion, n_views):
         files_of_taxonomy = []
         samples = self.get_samples_of_taxonomy(taxonomy_folder_name, dataset_portion)
+        n_samples = len(samples)
 
-        for sample_name in samples:
+        for sample_idx, sample_name in enumerate(samples):
             rendering_images_folder = self.rendering_image_path_template % (taxonomy_folder_name, sample_name)
 
-            file = {
-                'rendering_images': [],
-                'voxel': self.voxel_path_template % (taxonomy_folder_name, sample_name)
-            }
+            # Get meta data of rendering images
+            rendering_positions = []
+            with open(os.path.join(rendering_images_folder, 'rendering_metadata.txt'), encoding='utf-8') as file:
+                rendering_metadata = file.readlines()
+
+                for line_idx, line in enumerate(rendering_metadata):
+                    values = line.split(' ')
+                    if len(values) == 5:
+                        rendering_positions.append({
+                            'position': float(values[0]),
+                            'index': line_idx
+                        })
+            
+            if not len(rendering_positions) == 24:
+                print('[WARN] There are less than 24 rendering images for this 3D object: %s' % rendering_images_folder)
+            # Sort list of rendering images by positions
+            rendering_positions.sort(key=lambda x: x['position'])
+
             # Get file list of rendering images
-            for i in range(n_views):    # TODO, use random or other strategies according to viewpoints
-                file['rendering_images'].append(os.path.join(rendering_images_folder, '%02d.png' % (i + 1)))
+            selected_rendering_image_indexes = [int(x) for x in np.linspace(0, len(rendering_positions) - 1, num=n_views)]
+            selected_rendering_images = []
+            selected_rendering_image_positions = []
+            for image_idx in selected_rendering_image_indexes:
+                selected_rendering_images.append(os.path.join(rendering_images_folder, '%02d.png' % image_idx))
+                selected_rendering_image_positions.append(rendering_positions[image_idx])
 
-            # TODO: Meta data
+            # Append to the list of rendering images
+            files_of_taxonomy.append({
+                'rendering_images': selected_rendering_images,
+                'rendering_positions': selected_rendering_image_positions,
+                'voxel': self.voxel_path_template % (taxonomy_folder_name, sample_name)
+            })
 
-            files_of_taxonomy.append(file)
+            # Report the progress of reading dataset
+            if sample_idx % 500 == 499 or sample_idx == n_samples - 1:
+                print('[INFO] %s Collecting %d of %d' % (dt.now(), sample_idx + 1, n_samples))
 
         return files_of_taxonomy
 
