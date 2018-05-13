cb9382eb88038d64a1c4a906d90fb0af4b08db4c
Haozhe Xie <root@haozhexie.com>
2018-05-13 15:45:04 +0800

    Complete data loader for Pascal 3D.

--- config.py
+++ config.py
@@ -17,35 +17,44 @@ __C.CONST.RNG_SEED                      = 0
 __C.CONST.IMG_W                         = 224       # Image width for input
 __C.CONST.IMG_H                         = 224       # Image height for input
 __C.CONST.IMG_C                         = 3         # Image channels for input
-__C.CONST.CROP_IMG_W                    = 200
-__C.CONST.CROP_IMG_H                    = 200
-__C.CONST.CROP_IMG_C                    = 4
 __C.CONST.N_VOX                         = 32
 __C.CONST.N_VIEWS                       = 20
 __C.CONST.N_VIEWS_RENDERING             = 1
 __C.CONST.BATCH_SIZE                    = 24
+__C.CONST.CROP_IMG_W                    = 200       # Dummy property for Pascal 3D
+__C.CONST.CROP_IMG_H                    = 200       # Dummy property for Pascal 3D
+# For ShapeNet
+__C.CONST.CROP_IMG_C                    = 4
+# For Pascal3D
+# __C.CONST.CROP_IMG_C                  = 3
 
 #
 # Directories
 #
 __C.DIR                                 = edict()
-__C.DIR.DATASET_TAXONOMY_FILE_PATH      = './datasets/ShapeNet.json'
-# For ShapeNet
-# __C.DIR.DATASET_QUERY_PATH            = '/home/hzxie/Datasets/ShapeNet/ShapeNetRendering'
-# __C.DIR.VOXEL_PATH                    = '/home/hzxie/Datasets/ShapeNet/ShapeNetVox32/%s/%s.mat'
-# __C.DIR.RENDERING_PATH                = '/home/hzxie/Datasets/ShapeNet/ShapeNetRendering/%s/%s/render_%s.png'
-# For Pascal 3D
-__C.DIR.MESH_PATH                       = '/home/hzxie/Datasets/PASCAL3D/CAD/%s.mat'
-__C.DIR.RENDERING_PATH                  = '/home/hzxie/Datasets/PASCAL3D/Images/%s_imagenet/%s.JPEG'
 __C.DIR.OUT_PATH                        = './output'
+# For ShapeNet
+__C.DIR.DATASET_TAXONOMY_FILE_PATH      = './datasets/ShapeNet.json'
+__C.DIR.VOXEL_PATH                      = '/home/hzxie/Datasets/ShapeNet/ShapeNetVox32/%s/%s.mat'
+__C.DIR.RENDERING_PATH                  = '/home/hzxie/Datasets/ShapeNet/ShapeNetRendering/%s/%s/render_%s.png'
+# For Pascal 3D
+# __C.DIR.DATASET_TAXONOMY_FILE_PATH    = './datasets/Pascal3D.json'
+# __C.DIR.VOXEL_PATH                    = '/home/hzxie/Datasets/PASCAL3D/CAD/%s/%02d.binvox'
+# __C.DIR.ANNOTATION_PATH               = '/home/hzxie/Datasets/PASCAL3D/Annotations/%s_imagenet/%s.mat'
+# __C.DIR.RENDERING_PATH                = '/home/hzxie/Datasets/PASCAL3D/Images/%s_imagenet/%s.JPEG'
 
 #
 # Dataset
 #
 __C.DATASET                             = edict()
+# For ShapeNet
 __C.DATASET.DATASET_NAME                = 'ShapeNet'
 __C.DATASET.MEAN                        = [26.2284, 22.7098, 20.8072, 43.9129]
 __C.DATASET.STD                         = [0.0676, 0.0618, 0.0598, 0.0984]
+# For Pascal 3D
+# __C.DATASET.DATASET_NAME              = 'Pascal3D'
+# __C.DATASET.MEAN                      = [121.7832, 118.1967, 113.1437]
+# __C.DATASET.STD                       = [0.4232, 0.4206, 0.4345]
 
 #
 # Network
@@ -78,11 +87,11 @@ __C.TRAIN.RANDOM_BG_COLOR_RANGE         = [[225, 255], [225, 255], [225, 255]]
 __C.TRAIN.POLICY                        = 'adam'    # available options: sgd, adam
 __C.TRAIN.EPOCH_START_USE_REFINER       = 0
 __C.TRAIN.ENCODER_LEARNING_RATE         = .001
-__C.TRAIN.DECODER_LEARNING_RATE         = .0025
-__C.TRAIN.REFINER_LEARNING_RATE         = .0025
-__C.TRAIN.ENCODER_LR_MILESTONES         = [30, 60, 90, 120, 150, 180]
-__C.TRAIN.DECODER_LR_MILESTONES         = [30, 60, 90, 120, 150, 180]
-__C.TRAIN.REFINER_LR_MILESTONES         = [30, 60, 90, 120, 150, 180]
+__C.TRAIN.DECODER_LEARNING_RATE         = .001
+__C.TRAIN.REFINER_LEARNING_RATE         = .001
+__C.TRAIN.ENCODER_LR_MILESTONES         = [40]
+__C.TRAIN.DECODER_LR_MILESTONES         = [40]
+__C.TRAIN.REFINER_LR_MILESTONES         = [40]
 __C.TRAIN.BETAS                         = (.5, .5)
 __C.TRAIN.MOMENTUM                      = .9
 __C.TRAIN.VISUALIZATION_FREQ            = 10000     # visualization reconstruction voxels every visualization_freq batch
