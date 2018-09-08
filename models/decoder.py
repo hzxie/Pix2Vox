78d5ff2e4779407b77f3c148b1002e21617e9127
Haozhe Xie <root@haozhexie.com>
2018-09-08 16:08:34 +0800

    Replace average fusion with merger.

--- models/decoder.py
+++ models/decoder.py
@@ -40,22 +40,30 @@ class Decoder(torch.nn.Module):
         image_features = image_features.permute(1, 0, 2, 3, 4).contiguous()
         image_features = torch.split(image_features, 1, dim=0)
         gen_voxels     = []
+        raw_features   = []
 
         for features in image_features:
-            gen_voxel = features.view(-1, 2048, 2, 2, 2)
+            gen_voxel    = features.view(-1, 2048, 2, 2, 2)
             # print(gen_voxel.size())   # torch.Size([batch_size, 2048, 2, 2, 2])
-            gen_voxel = self.layer1(gen_voxel)
+            gen_voxel    = self.layer1(gen_voxel)
             # print(gen_voxel.size())   # torch.Size([batch_size, 512, 4, 4, 4])
-            gen_voxel = self.layer2(gen_voxel)
+            gen_voxel    = self.layer2(gen_voxel)
             # print(gen_voxel.size())   # torch.Size([batch_size, 128, 8, 8, 8])
-            gen_voxel = self.layer3(gen_voxel)
+            gen_voxel    = self.layer3(gen_voxel)
             # print(gen_voxel.size())   # torch.Size([batch_size, 32, 16, 16, 16])
-            gen_voxel = self.layer4(gen_voxel)
+            gen_voxel    = self.layer4(gen_voxel)
+            raw_feature  = gen_voxel
             # print(gen_voxel.size())   # torch.Size([batch_size, 8, 32, 32, 32])
-            gen_voxel = self.layer5(gen_voxel)
+            gen_voxel    = self.layer5(gen_voxel)
             # print(gen_voxel.size())   # torch.Size([batch_size, 1, 32, 32, 32])
-            gen_voxels.append(torch.squeeze(gen_voxel, 1))
+            raw_feature  = torch.cat((raw_feature, gen_voxel), dim=1)
+            # print(raw_feature.size()) # torch.Size([batch_size, 9, 32, 32, 32])
+            
+            gen_voxels.append(torch.squeeze(gen_voxel, dim=1))
+            raw_features.append(raw_feature)
 
-        gen_voxels = torch.stack(gen_voxels).permute(1, 0, 2, 3, 4).contiguous()
-        # print(gen_voxels.size())      # torch.Size([batch_size, n_views, 32, 32, 32])
-        return torch.mean(gen_voxels, dim=1)
+        gen_voxels   = torch.stack(gen_voxels).permute(1, 0, 2, 3, 4).contiguous()
+        raw_features = torch.stack(raw_features).permute(1, 0, 2, 3, 4, 5).contiguous()
+        # print(gen_voxels.size())        # torch.Size([batch_size, n_views, 32, 32, 32])
+        # print(raw_features.size())      # torch.Size([batch_size, n_views, 9, 32, 32, 32])
+        return raw_features, gen_voxels
---
+++ models/merger.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+# Developed by Haozhe Xie <cshzxie@gmail.com>
+
+import torch
+
+class Merger(torch.nn.Module):
+    def __init__(self, cfg):
+        super(Merger, self).__init__()
+        self.cfg = cfg
+
+        # Layer Definition
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv3d(9, 16, kernel_size=3, padding=1),
+            torch.nn.BatchNorm3d(16),
+            torch.nn.LeakyReLU(cfg.NETWORK.LEAKY_VALUE)
+        )
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv3d(16, 8, kernel_size=3, padding=1),
+            torch.nn.BatchNorm3d(8),
+            torch.nn.LeakyReLU(cfg.NETWORK.LEAKY_VALUE)
+        )
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv3d(8, 4, kernel_size=3, padding=1),
+            torch.nn.BatchNorm3d(4),
+            torch.nn.LeakyReLU(cfg.NETWORK.LEAKY_VALUE)
+        )
+        self.layer4 = torch.nn.Sequential(
+            torch.nn.Conv3d(4, 2, kernel_size=3, padding=1),
+            torch.nn.BatchNorm3d(2),
+            torch.nn.LeakyReLU(cfg.NETWORK.LEAKY_VALUE)
+        )
+        self.layer5 = torch.nn.Sequential(
+            torch.nn.Conv3d(2, 1, kernel_size=3, padding=1),
+            torch.nn.BatchNorm3d(1),
+            torch.nn.LeakyReLU(cfg.NETWORK.LEAKY_VALUE)
+        )
+
+    def forward(self, raw_features, coarse_voxels):
+        raw_features     = torch.split(raw_features, 1, dim=1)
+        voxel_weights    = []
+        
+        for i in range(self.cfg.CONST.N_VIEWS_RENDERING):
+            raw_feature  = torch.squeeze(raw_features[i], dim=1)
+            # print(raw_feature.size())       # torch.Size([batch_size, 9, 32, 32, 32])
+            
+            voxel_weight = self.layer1(raw_feature)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 16, 32, 32, 32])
+            voxel_weight = self.layer2(voxel_weight)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 8, 32, 32, 32])
+            voxel_weight = self.layer3(voxel_weight)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 4, 32, 32, 32])
+            voxel_weight = self.layer4(voxel_weight)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 2, 32, 32, 32])
+            voxel_weight = self.layer5(voxel_weight)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 1, 32, 32, 32])
+
+            voxel_weight = torch.squeeze(voxel_weight, dim=1)
+            # print(voxel_weight.size())      # torch.Size([batch_size, 32, 32, 32])
+            voxel_weights.append(voxel_weight)
+
+        voxel_weights    = torch.stack(voxel_weights).permute(1, 0, 2, 3, 4).contiguous()
+        voxel_weights    = torch.softmax(voxel_weights, dim=1)
+        # print(voxel_weights.size())         # torch.Size([batch_size, n_views, 32, 32, 32])
+        # print(coarse_voxels.size())         # torch.Size([batch_size, n_views, 32, 32, 32])
+        coarse_voxels    = coarse_voxels * voxel_weights
+        coarse_voxels    = torch.sum(coarse_voxels, dim=1)
+
+        return torch.clamp(coarse_voxels, min=0, max=1)
