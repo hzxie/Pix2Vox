3ff3a3cad9080f368a32cc269d17672f64df573d
Haozhe Xie <root@haozhexie.com>
2018-10-04 12:14:56 +0800

    Remove unsed shebang in files.

--- models/encoder.py
+++ models/encoder.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Developed by Haozhe Xie <cshzxie@gmail.com>
--- utils/binvox_converter.py
+++ utils/binvox_converter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # Developed by Haozhe Xie <cshzxie@gmail.com>
