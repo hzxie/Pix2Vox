9b9ae8e0b44fb7ceb8a61432a0b7e91b33f268df
sroy <sidhartha.roy@outlook.com>
2020-02-12 14:15:02 -0800

    stremalit download model 2

--- streamlit.py
+++ streamlit.py
@@ -23,26 +23,6 @@ def generate_data():
     volume = volume.squeeze().__ge__(0.5)
     return volume
 
-# let's put sliders to modify view init, each time you move that the script is rerun, but voxels are not regenerated
-
-# TODO : not sure that's the most optimized way to rotate axis but well, demo purpose
-
-azim = st.sidebar.slider("azim", 0, 180, 30, 1)
-
-elev = st.sidebar.slider("elev", 0, 360, 240, 1)
-
-# Download Data
-download_model_from_web()
-
-# and plot everything
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-volume = generate_data()
-ax.voxels(volume, facecolors='r', edgecolor='k')
-ax.view_init(azim, elev)
-ax.axis('off')
-st.pyplot()
-
 MODEL_FILENAME = 'pretrained_model/Pix2Vox-A-ShapeNet.pth'
 MODEL_DIR = 'pretrained_model'
 
@@ -63,5 +43,25 @@ def download_model_from_web():
     with open(MODEL_FILENAME, 'wb') as file_desc:
         for chunk in resp.iter_content(chunk_size=5000000):
             file_desc.write(chunk)
+
+# let's put sliders to modify view init, each time you move that the script is rerun, but voxels are not regenerated
+
+# TODO : not sure that's the most optimized way to rotate axis but well, demo purpose
+
+azim = st.sidebar.slider("azim", 0, 180, 30, 1)
+
+elev = st.sidebar.slider("elev", 0, 360, 240, 1)
+
+# Download Data
+download_model_from_web()
+
+# and plot everything
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+volume = generate_data()
+ax.voxels(volume, facecolors='r', edgecolor='k')
+ax.view_init(azim, elev)
+ax.axis('off')
+st.pyplot()
 
 
